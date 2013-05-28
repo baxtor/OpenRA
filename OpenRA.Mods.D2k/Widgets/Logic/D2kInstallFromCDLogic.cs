3ea998c1bce040fdb46a986b85f8bdb0b479c3d8
baxtor <the_unknown_space@freenet.de>
2013-05-28 21:27:16 +0200

    uncommented extraction process code to extract .R8 files from setup.z

--- OpenRA.Mods.D2k/Widgets/Logic/D2kInstallFromCDLogic.cs
+++ OpenRA.Mods.D2k/Widgets/Logic/D2kInstallFromCDLogic.cs
@@ -78,15 +78,16 @@ namespace OpenRA.Mods.D2k.Widgets.Logic
 			insertDiskContainer.IsVisible = () => false;
 			installingContainer.IsVisible = () => true;
 
-			var dest = new string[] { Platform.SupportDir, "Content", "d2k", "Music" }.Aggregate(Path.Combine);
+			var destMusic = new string[] { Platform.SupportDir, "Content", "d2k", "Music" }.Aggregate(Path.Combine);
+			var destData = new [] { Platform.SupportDir, "Content", "d2k" }.Aggregate (Path.Combine);
 			var copyFiles = new string[] { "music/ambush.aud", "music/arakatak.aud", "music/atregain.aud", "music/entordos.aud", "music/fightpwr.aud", "music/fremen.aud", "music/hark_bat.aud", "music/landsand.aud", "music/options.aud", "music/plotting.aud", "music/risehark.aud", "music/robotix.aud", "music/score.aud", "music/soldappr.aud", "music/spicesct.aud", "music/undercon.aud", "music/waitgame.aud" };
 
 			// TODO: won't work yet:
-			//var extractPackage = "setup/setup.z";
-			//var extractFiles = new string[] { "DATA.R8", "MOUSE.R8", "BLOXBASE.R8", "BLOXBAT.R8", "BLOXBGBS.R8", "BLOXICE.R8", "BLOXTREE.R8", "BLOXWAST.R8" };
+			var extractPackage = "setup/setup.z";
+			var extractFiles = new string[] { "DATA.R8", "MOUSE.R8", "BLOXBASE.R8", "BLOXBAT.R8", "BLOXBGBS.R8", "BLOXICE.R8", "BLOXTREE.R8", "BLOXWAST.R8" };
 
 			var installCounter = 0;
-			var installTotal = copyFiles.Count(); //+ extractFiles.Count();
+			var installTotal = copyFiles.Count() + extractFiles.Count();
 
 			var onProgress = (Action<string>)(s => Game.RunAfterTick(() =>
 			{
@@ -107,11 +108,11 @@ namespace OpenRA.Mods.D2k.Widgets.Logic
 			{
 				try
 				{
-					if (!InstallUtils.CopyFiles(source, copyFiles, dest, onProgress, onError))
+					if (!InstallUtils.CopyFiles(source, copyFiles, destMusic, onProgress, onError))
 						return;
 
-					//if (!InstallUtils.ExtractFromPackage(source, extractPackage, extractFiles, dest, onProgress, onError))
-					//	return;
+					if (!InstallUtils.ExtractFromPackage(source, extractPackage, extractFiles, destData, onProgress, onError))
+						return;
 
 					Game.RunAfterTick(() =>
 					{
